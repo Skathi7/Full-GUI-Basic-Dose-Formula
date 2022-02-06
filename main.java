2534c97a12e6500ba8af6b663c8cc4ee9db3165d
Skathi7 <49845642+Skathi7@users.noreply.github.com>
2022-02-06 13:28:44 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	
+	/*
+	 * This is meant to be a super simple java dose calculator displayed in the swing and have a full GUI, 
+	 * it uses the universal dose formula to calculate a dose based on the user's inputed values. 
+	 * I do not recommend using this to actually calculate real world doses of course, 
+	 * this is more meant as a demonstration of the code
+	 * 
+	 * This program is not completed and still in the works
+	 */
+	public static void main(String[] args) {
+		
+	}//End of main function
+}
